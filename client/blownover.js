6e488388f7e518363bf6be40a68beaea06cf6a77
Dan Harper <intouch@danharper.me>
2013-09-17 12:06:57 +0100

    add location bar

--- client/blownover.js
+++ client/blownover.js
@@ -1,10 +1,14 @@
 var map;
 
-var addPoint = function(latLng) {
+var addPoint = function(latLng, opts) {
+	opts = opts || {};
+
+	var title = opts.address + ' - ' + opts.rating + ' - ' + opts.message;
+
 	return new google.maps.Marker({
 		position: latLng,
 		map: map,
-		title: 'New Zombie Location!'
+		title: title
 	})
 };
 
@@ -16,6 +20,32 @@ Template.hello.events({
 	'click input' : function () {
 		// template data, if any, is available in 'this'
 		console.log("You pressed the button");
+	}
+});
+
+
+Template.input.events({
+	'click button': function() {
+		console.log('add', this);
+		var location, rating, message;
+		location = $('#input-postcode').val();
+		rating = parseInt($('#input-rating').val(), 10) || 1;
+		message = $('#input-message').val();
+
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({ 'address': location }, function(result, status) {
+			console.log('res', result);
+			if (status == google.maps.GeocoderStatus.OK) {
+				addPoint(result[0].geometry.location, {
+					address: result[0].formatted_address,
+					message: message,
+					rating: rating
+				});
+			}
+			else {
+				alert('Invalid Geocoder Request: '+status);
+			}
+		});
 	}
 });
 
@@ -32,7 +62,11 @@ Template.map.rendered = function() {
 
 	setTimeout(function() {
 		var myLatlng = new google.maps.LatLng(50.800999, -1.090736);
-		addPoint(myLatlng);
+		addPoint(myLatlng, {
+			address: '',
+			message: 'New Zombie Location!',
+			rating: 8
+		});
 	}, 8000);
 }
 
